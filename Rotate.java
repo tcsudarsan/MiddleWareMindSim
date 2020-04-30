c0c66ec0b752488c1581becc3accbda355b278d6
chay <tholeti@ecbm.me>
2020-04-30 10:15:14 +0200

    Update Rotate.java
    Beautify the code

--- Rotate.java
+++ Rotate.java
@@ -1,5 +1,6 @@
+//initialise the package for Vrep simulation tool
 package coppelia;
-
+//initalise a class to rotate the Legos mind storm simulator
 public class Rotate {
 void Rotate(int clientID,float speed){
 	remoteApi vrep=new remoteApi();
@@ -9,6 +10,7 @@ void Rotate(int clientID,float speed){
 	FloatWA eulerAngles=new FloatWA(1);
 	int leftHandleMotor=0;
 	int rightHandleMotor=0;
+	//block the movement of right motor of simulator to rotate the Legosmind storm simulator
 	leftHandleMotor=vrep.simxGetObjectHandle(clientID, "Pioneer_p3dx_leftMotor", leftHandle, vrep.simx_opmode_blocking);
 	//System.out.println(leftHandle.getValue());
     //rightHandleMotor=vrep.simxGetObjectHandle(clientID, "Pioneer_p3dx_rightMotor", rightHandle, vrep.simx_opmode_blocking);
